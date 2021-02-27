837b19f6b744608eba095baa764c9b81bc84928c
Timid Robot Zehta <timidrobot@zehta.me>
2021-02-27 11:43:55 -0800

    update Python tooling

--- tidy.sh
+++ tidy.sh
@@ -11,12 +11,14 @@ trap '_es=${?};
 
 for _file in $(find docs -type f -name '*.html')
 do
+    result=$(tidy -errors -indent -wrap 80 -quiet -utf8 \
+                --drop-empty-elements no \
+                --drop-empty-paras no \
+                --quote-ampersand no \
+                --quote-nbsp no \
+                "${_file}" 2>&1 || true)
+    [[ -n "${result}" ]] || continue
     printf "\e[1m\e[7m %-80s\e[0m\n" "${_file}"
-    tidy -modify -indent -wrap 80 -quiet -utf8 \
-        --drop-empty-elements no \
-        --drop-empty-paras no \
-        --quote-ampersand no \
-        --quote-nbsp no \
-        "${_file}" || true
+    echo "${result}"
     echo
 done
--- tools.sh
+++ tools.sh
@@ -10,13 +10,9 @@ trap '_es=${?};
     exit ${_es}' ERR
 
 printf "\e[1m\e[7m %-80s\e[0m\n" 'isort'
-pipenv run isort --check ${@:-.}
+pipenv run isort ${@:-.}
 echo
 
 printf "\e[1m\e[7m %-80s\e[0m\n" 'black'
-pipenv run black --check ${@:-.}
-echo
-
-printf "\e[1m\e[7m %-80s\e[0m\n" 'flake8'
-pipenv run flake8 ${@:-.}
+pipenv run black ${@:-.}
 echo
