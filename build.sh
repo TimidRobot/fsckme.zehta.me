792d08fc7a83f8346a539909145153a612e4a13d
Timid Robot Zehta <timidrobot@zehta.me>
2021-04-17 12:24:52 -0700

    do not reinstall plugins by default

--- build.sh
+++ build.sh
@@ -10,15 +10,17 @@ trap '_es=${?};
     exit ${_es}' ERR
 
 printf "\e[1m\e[7m %-80s\e[0m\n" 'Removing contents of build dir (docs/)'
-rm -rf docs/*
-echo
-echo
-printf "\e[1m\e[7m %-80s\e[0m\n" 'Lektor: uninstalling all plugins'
-pipenv run lektor plugins flush-cache
-echo
-echo
-printf "\e[1m\e[7m %-80s\e[0m\n" 'Lektor: installing all plugins'
-pipenv run lektor plugins reinstall
+echo "$(rm -rfv docs/* | wc -l) files and directories removed"
+if [[ "${1:-}" == '-p' ]]; then
+    echo
+    echo
+    printf "\e[1m\e[7m %-80s\e[0m\n" 'Lektor: uninstalling all plugins'
+    pipenv run lektor plugins flush-cache
+    echo
+    echo
+    printf "\e[1m\e[7m %-80s\e[0m\n" 'Lektor: installing all plugins'
+    pipenv run lektor plugins reinstall
+fi
 echo
 echo
 printf "\e[1m\e[7m %-80s\e[0m\n" 'Lektor: building site'
