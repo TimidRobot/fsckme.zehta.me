68cabe3178c830f144d12f665a03a4b5c4a63b63
Timid Robot Zehta <timidrobot@zehta.me>
2021-12-16 22:37:22 -0800

    remove error handling

--- tools.sh
+++ tools.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 #### SETUP ####################################################################
-set -o errexit
 set -o errtrace
 set -o nounset
-
-trap '_es=${?};
-    printf "${0}: line ${LINENO}: \"${BASH_COMMAND}\"";
-    printf " exited with a status of ${_es}\n";
-    exit ${_es}' ERR
 
 printf "\e[1m\e[7m %-80s\e[0m\n" 'isort'
 pipenv run isort ${@:-.}
